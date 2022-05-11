4110cfc2085b9c7e56ce9276b36fc732008ef45d
RealFiction <76532627+RealFictionGO@users.noreply.github.com>
2022-05-11 15:59:58 +0000

    adding game's files

--- main.py
+++ main.py
@@ -9,25 +9,17 @@ def home():
 def library():
   return render_template("library.html")
 
-@app.route('/library/snake')
-def snake():
-  return render_template("snake_site.html")
-
-@app.route('/library/snake/game')
-def play_snake():
-  return render_template("")
-
-@app.route('/library/pac-man')
+@app.route('/pacman')
 def pac_man():
-  return render_template("pac_man_site.html")
+  return render_template("pacman.html")
 
-@app.route('/library/pac-man/game')
-def play_pac_man():
-  return render_template("")
+@app.route('/snake')
+def snake():
+  return render_template("snake.html")
 
-@app.route('/library/tetris/game')
-def play_tetris():
-  return render_template("")
+@app.route('/tetris')
+def tetris():
+  return render_template("tetris.html")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080, debug=True)
