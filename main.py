94b91df83609cf79f589cba57dc249054eca8e3b
RealFiction <76532627+RealFictionGO@users.noreply.github.com>
2022-05-16 17:46:47 +0000

    rest of site

--- main.py
+++ main.py
@@ -3,31 +3,39 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
-  return render_template("index.html")
+  return render_template("glowna.html")
 
-@app.route('/library')
+@app.route('/sources')
+def sources():
+  return render_template("zrodlawykorzystane.html")
+
+@app.route('/libraryofgames')
 def library():
-  return render_template("library.html")
+  return render_template("bibliotekagier.html")
 
-@app.route('/library/pacman')
+@app.route('/libraryofgames/pacman')
+def pac_man_site():
+  return render_template("pacmanstrona.html")
+
+@app.route('/libraryofgames/pacman/game')
 def pac_man():
   return render_template("pacman.html")
 
-@app.route('/library/snake')
+@app.route('/libraryofgames/snake')
 def snake_page():
-  return render_template("snakestron.html")
+  return render_template("snakestrona.html")
 
-@app.route('/library/snake/game')
+@app.route('/libraryofgames/snake/game')
 def snake():
   return render_template("snake.html")
 
-@app.route('/library/tetris')
+@app.route('/libraryofgames/tetris')
 def tetris_page():
   return render_template("tetrisstrona.html")
 
-@app.route('/library/tetris/game')
+@app.route('/libraryofgames/tetris/game')
 def tetris():
   return render_template("tetris.html")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(port=8080, debug=True)
